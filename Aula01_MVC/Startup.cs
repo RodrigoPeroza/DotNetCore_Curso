906c10e783c15f6240304682ffe595220f29d4fe
Rodrigo Peroza <rperoza@videplast.local>
2019-07-12 14:25:47 -0300

    Ok

--- Aula01_MVC/Startup.cs
+++ Aula01_MVC/Startup.cs
@@ -41,6 +41,8 @@ namespace Aula01_MVC
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            Console.WriteLine($"Banco {Configuration["ConnectionString"]}");
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
